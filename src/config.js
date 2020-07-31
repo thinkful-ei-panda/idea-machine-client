b1d0319e2f7d5892b432ca3d8b9ff32622119fca
Wesley Rou <wesleyrou@gmail.com>
2020-07-30 19:41:23 -0700

    config endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://nameless-bayou-94377.herokuapp.com/api',
   TOKEN_KEY: 'idea-machine-client-auth-token',
 
 }
